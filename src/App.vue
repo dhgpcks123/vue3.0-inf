88b7d508b831ba2c3908dfd50efbc085e1cbd713
hyechan <dhgpcks123@naver.com>
2022-01-12 17:11:57 +0900

    git init & v-bind, v-on

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div v-bind:class="nameClass"> v-bind: -> : 로 사용 가능하다.-->
+  <div :class="nameClass">
+    {{ name }}
+  </div>
+  <input v-bind:type="type" v-bind:value="name" v-bind:placeholder="test">
+  <button
+    class="btn btn-primary"
+    @click="updateName"
+  >
+  <!-- v-on:click="updateName"  v-on:click -> @click -->
+    Click
+  </button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  setup() {
+    const name = ref('hyechan')
+    const test = ref('chat here')
+    const type = ref('text')
+    const nameClass = ref('')
+
+    const updateName = () => {
+      name.value = '5'
+      test.value = '숫자를 입력해주세요'
+      type.value = 'number'
+      nameClass.value = 'name'
+    }
+    return {
+      name, updateName, test, type, nameClass
+    }
   }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.name{
+  color: red
 }
 </style>
