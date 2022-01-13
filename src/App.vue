3fb1ea4a413e09d324cdaba97b1b87b08e118773
hyechan <dhgpcks123@naver.com>
2022-01-13 18:18:22 +0900

    pagination 구현 및 컴포넌트화

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,12 @@
       @toggle-todo="toggleTodo" 
       @delete-todo="deleteTodo"
     />
+    <hr>
+    <Pagination
+      :currentPage="currentPage"
+      :numberOfPages="numberOfPages"
+      @get-todos="getTodos"
+    />
   </section>
 </template>
 
@@ -27,17 +33,40 @@
 import { ref, computed } from 'vue'
 import TodoSimpleForm from './components/TodoSimpleForm.vue'
 import TodoList from './components/TodoList.vue'
+import Pagination from './components/Pagination.vue'
 import axios from 'axios'
 
 export default{
   components :{
-    TodoSimpleForm, TodoList,
+    TodoSimpleForm, TodoList, Pagination,
   },
   setup() {
     const toggle = ref(false)
     const todos = ref([])
     const searchText = ref('')
     const error = ref('')
+    const numberOfTodos = ref(0)
+    const limit = 5
+    const currentPage = ref(1)
+
+    const numberOfPages = computed(()=>{
+      return Math.ceil(numberOfTodos.value/limit)
+    })
+
+    const getTodos = async (page = currentPage.value) => {
+      // es6문법인가봄. page 받는데 디폴트 값 currentPage.value인가봐.
+      currentPage.value = page
+      try{
+        const res = await axios.get(`http://localhost:3000/todos?_page=${page}&_limit=${limit}`)
+        numberOfTodos.value = res.headers['x-total-count']
+        todos.value = res.data
+      }catch(err){
+        error.value = 'Something went wrong.'
+        console.log(err)
+      }
+    }
+
+    getTodos();
 
     const addTodo = async (todo) => {
       // db 저장
@@ -47,25 +76,35 @@ export default{
           subject: todo.subject,
           completed: todo.completed,
         })
-        todos.value.push(res)
+        todos.value.push(res.data)
+      }catch(err){
+        error.value = 'Something went wrong.'
+        console.log(err)
+      } 
+    }
+    const toggleTodo = (index) => {
+      error.value = ''
+      const id = todos.value[index].id
+      try{
+        axios.patch(`http://localhost:3000/todos/${id}`, {
+          completed: !todos.value[index].completed
+        })
+        todos.value[index].completed = !todos.value[index].completed
       }catch(err){
         error.value = 'Something went wrong.'
         console.log(err)
       }
-      // .then(res => {
-      //   console.log(res)
-      //   todos.value.push(res.data)
-      // }).catch(err => {
-      //   error.value = 'Something went wrong.'
-      //   console.log(err)
-      // })
-      console.log('hello')
     }
-    const toggleTodo = (index) => {
-      todos.value[index].completed = !todos.value[index].completed
-    }
-    const deleteTodo = (index) => {
+    const deleteTodo = async (index) => {
+      error.value = ''
+      const id = todos.value[index].id
       todos.value.splice(index, 1)
+      try{
+        await axios.delete(`http://localhost:3000/todos/${id}`)
+      }catch(err){
+        error.value = 'Something went wrong.'
+        console.log(err)
+      }
     }
     const filteredTodos = computed(()=>{
       if(searchText.value){
@@ -77,9 +116,9 @@ export default{
       return todos.value
     })
     return {
-      todos, toggle,
-      deleteTodo, toggleTodo, addTodo,
-      searchText, filteredTodos, error,
+      todos, toggle, numberOfTodos,
+      deleteTodo, toggleTodo, addTodo, getTodos, numberOfPages,
+      searchText, filteredTodos, error, currentPage
     }
   }
 }
---
+++ src/components/Pagination.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-if="currentPage !== 1" class="page-item">
+          <a
+            @click="getTodos(currentPage-1)"
+            class="page-link"
+          >
+            Previous
+          </a>
+        </li>
+        <li
+          v-for="page in numberOfPages" :key="page"
+          class="page-item"
+          :class="currentPage === page ? 'active' : ''"
+        >
+          <a @click="getTodos(page)" class="page-link">{{page}}</a>
+        </li>
+        <li v-if="numberOfPages!=0 && numberOfPages !== currentPage" class="page-item">
+          <a
+            @click="getTodos(currentPage+1)"
+            class="page-link"
+          >
+          next
+          </a>
+        </li>
+      </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+    numberOfPages: {
+      type: Number,
+      required: true,
+    }
+  },
+  setup(context, { emit }){
+    const getTodos = (page)=>{
+      emit('get-todos', page)
+    }
+    return {
+      getTodos
+      }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
